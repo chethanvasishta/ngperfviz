0d73505d0fce44b543a3a39312f6a16a62e01c5e
chethan.ravindranath <chethan.vasishta@gmail.com>
2017-01-21 10:54:00 +0530

    Adding a view and a template for home page

--- indri/views.py
+++ indri/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from indri.models import Product
 
 def index(request):
-	return HttpResponse("Chethan!")
+	products = Product.objects.all()
+	context = {'product_list' : products}
+	return render(request, 'indri/index.html', context)
+
+def detail(request, product_id):
+	return index(request)
+	
