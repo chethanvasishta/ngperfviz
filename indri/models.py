73ca4a746e5adcca3cdfc0bdef3db2e5e5d6b093
chethan.ravindranath <chethan.vasishta@gmail.com>
2017-01-22 10:56:59 +0530

    Adding a few more fields to the test class, creating a base class and moving them to the base class

--- indri/models.py
+++ indri/models.py
@@ -18,16 +18,23 @@ class Release(models.Model):
 class Build(models.Model):
 	release = models.ForeignKey(Release, on_delete = models.CASCADE)
 	buildID = models.CharField(max_length = 256)
-
+	
 # Each build has a set of RuntimeTestRuns, CompileTimeTestRuns etc.
-class RunTimeTestRun(models.Model):
-	build = models.ForeignKey(Build, on_delete = models.CASCADE)
-	runTimes = models.CharField(max_length = 2560) # this would be comma separated double values of the test run outputs. We would need this for getting statistics
-	tag = models.CharField(max_length = 256) # This would a feature name tag, for. e.g. cleanup, vif etc.
+class TestRun(models.Model):
+	build 		= models.ForeignKey(Build, on_delete = models.CASCADE)
+	testName 	= models.CharField(max_length = 1024) 	# used to compare against previous builds
+	runTimes 	= models.CharField(max_length = 2560) 	# comma separated double values of the test run outputs. We would need this for getting statistics
+	tag 		= models.CharField(max_length = 256) 	# feature name tag, for. e.g. cleanup, vif etc.
+	mean 		= models.DecimalField(max_digits = 8, decimal_places = 2)
+	median 		= models.DecimalField(max_digits = 8, decimal_places = 2)
+	
+	class Meta:
+		abstract = True	# prevents this class from being converted into a table in the database
 
-class CompileTimeTestRun(models.Model):
-	build = models.ForeignKey(Build, on_delete = models.CASCADE)
-	compileTimes = models.CharField(max_length = 2560) # this would be comma separated double values of the test run outputs. We would need this for getting statistics
-	tag = models.CharField(max_length = 256) # This would a feature name tag, for. e.g. cleanup, vif etc.
+class RunTimeTestRun(TestRun): pass
+	
+
+class CompileTimeTestRun(TestRun): pass
+	
 	
 # class MemoryTestRun(models.Model):
